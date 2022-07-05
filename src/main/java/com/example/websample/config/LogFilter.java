30ef6e265929891048c0fb64bd1a91c80316ab1d
You <duug@naver.com>
2022-07-05 17:56:02 +0900

    filter 추가

---
+++ src/main/java/com/example/websample/config/LogFilter.java
@@ -0,0 +1,20 @@
+package com.example.websample.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+@Slf4j
+public class LogFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+        log.info("Hello LogFilter : " + Thread.currentThread());
+        // 외부 -> filter (-> 처리 ->) filter -> 외부
+        // 필터가 여러개인 경우 필터와 필터를 연결해주기 위해 꼭 필요한 역할을 함
+        chain.doFilter(request, response);
+        log.info("Bye LogFilter : " + Thread.currentThread());
+    }
+}
